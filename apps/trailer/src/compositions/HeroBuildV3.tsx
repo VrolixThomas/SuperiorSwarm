c12e68bb4a437e6f665dec84a1cc45627886d887
Thomas <43751036+VrolixThomas@users.noreply.github.com>
2026-05-16 09:55:57 +0200

    feat(trailer-v3): six-beat workspace with edge-to-edge framing

--- apps/trailer/src/compositions/HeroBuildV3.tsx
+++ apps/trailer/src/compositions/HeroBuildV3.tsx
@@ -1,8 +1,10 @@
 import { AbsoluteFill, Sequence } from "remotion";
 import { Bg } from "../hero/Bg";
+import { CameraV3 } from "../hero/build-v3/Camera";
 import { CaptionV3 } from "../hero/build-v3/Caption";
 import { ColdOpenV3 } from "../hero/build-v3/ColdOpenV3";
 import { CollapseV3 } from "../hero/build-v3/CollapseV3";
+import { WorkspaceV3 } from "../hero/build-v3/WorkspaceV3";
 import { ACTS_V3, TOTAL_FRAMES_V3 } from "../hero/build-v3/timeline";
 
 export const HERO_BUILD_V3_FRAMES = TOTAL_FRAMES_V3;
@@ -19,6 +21,9 @@ export function HeroBuildV3() {
 				<ColdOpenV3 />
 			</Sequence>
 			<CollapseV3 />
+			<CameraV3>
+				<WorkspaceV3 />
+			</CameraV3>
 			<CaptionV3 />
 		</AbsoluteFill>
 	);
